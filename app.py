74af9ce322443d7d75f6822f336a658d2a930f19
Jude Joseph Agustino <mainjudejoseph@gmail.com>
2025-09-09 08:25:51 +0800

    update app.py

--- app.py
+++ app.py
@@ -345,6 +345,6 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Prediction error: {str(e)}")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8002)
